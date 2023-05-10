a45429f575f417dff45bb4a6392e752288ec2481
cidkidnix <cidkidnix@protonmail.com>
2023-05-10 12:10:24 -0400

    Update release.nix to include testing for workflow modules

--- release.nix
+++ release.nix
@@ -1,6 +1,11 @@
 let
   damlSdk = import ./dep/nix-daml-sdk {};
   pkgs = import ./dep/nixpkgs {};
+  # Checks to make sure were only importing a subset of modules from daml-finance
+  excludeWorkflowModules = builtins.concatStringsSep "|" [
+    "import Daml.Finance.Interface.*"
+    "import Daml.Finance.Claims.*"
+  ];
 in
   damlSdk.reflex-platform.nixpkgs.stdenv.mkDerivation {
     name = "syndicated-loan";
@@ -11,5 +16,6 @@ in
       mkdir $out
       ${damlSdk.sdk}/bin/daml test
       ${damlSdk.sdk}/bin/daml build -o $out/package.dar
+      find $src/daml/Workflow -name "*.daml" -type f -print0 | xargs -0 grep -o "import Daml.Finance.*" | grep -Ev "${excludeWorkflowModules}"
     '';
 }
